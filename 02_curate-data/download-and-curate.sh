1b745a948543166c00588f7dfdfa109bd17a5425
Alexandra McIsaac <armcisaac@gmail.com>
2024-03-04 11:33:47 -0800

    adding files related to fit with no heteroatom r4 parameter

--- 02_curate-data/download-and-curate.sh
+++ 02_curate-data/download-and-curate.sh
@@ -36,30 +36,30 @@ python curate-dataset.py download-opt                                           
 
 
 
-#python curate-dataset.py download-td                                                \
-#    --core-td-dataset       "OpenFF Gen 2 Torsion Set 1 Roche 2"                    \
-#    --core-td-dataset       "OpenFF Gen 2 Torsion Set 2 Coverage 2"                 \
-#    --core-td-dataset       "OpenFF Gen 2 Torsion Set 3 Pfizer Discrepancy 2"       \
-#    --core-td-dataset       "OpenFF Gen 2 Torsion Set 4 eMolecules Discrepancy 2"   \
-#    --core-td-dataset       "OpenFF Gen 2 Torsion Set 5 Bayer 2"                    \
-#    --core-td-dataset       "OpenFF Gen 2 Torsion Set 6 supplemental 2"             \
-#    --aux-td-dataset        "SMIRNOFF Coverage Torsion Set 1"                       \
-#    --aux-td-dataset        "OpenFF Group1 Torsions"                                \
-#    --aux-td-dataset        "OpenFF Group1 Torsions 2"                              \
-#    --aux-td-dataset        "OpenFF Group1 Torsions 3"                              \
-#    --aux-td-dataset        "Pfizer discrepancy torsion dataset 1"                  \
-#    --aux-td-dataset        "OpenFF Gen3 Torsion Set v1.0"                          \
-#    --aux-td-dataset        "OpenFF Amide Torsion Set v1.0"                         \
-#    --aux-td-dataset        "OpenFF WBO Conjugated Series v1.0"                     \
-#    --aux-td-dataset        "OpenFF DANCE 1 eMolecules t142 v1.0"                   \
-#    --initial-forcefield    "../01_generate-forcefield/output/initial-force-field.offxml" \
-#    --td-records-to-remove  "td_records_to_remove.dat"                              \
-#    --additional-td-records "additional_td_records.json"                            \
-#    --cap-size              5                                                       \
-#    --cap-method            "pick_random"                                           \
-#    --n-processes           8                                                       \
-#    --output                "output/torsion-training-set.json"                      \
-#    --output-parameter-smirks "output/training-torsion-smirks.json"                 \
-#    --verbose
+python curate-dataset.py download-td                                                \
+    --core-td-dataset       "OpenFF Gen 2 Torsion Set 1 Roche 2"                    \
+    --core-td-dataset       "OpenFF Gen 2 Torsion Set 2 Coverage 2"                 \
+    --core-td-dataset       "OpenFF Gen 2 Torsion Set 3 Pfizer Discrepancy 2"       \
+    --core-td-dataset       "OpenFF Gen 2 Torsion Set 4 eMolecules Discrepancy 2"   \
+    --core-td-dataset       "OpenFF Gen 2 Torsion Set 5 Bayer 2"                    \
+    --core-td-dataset       "OpenFF Gen 2 Torsion Set 6 supplemental 2"             \
+    --aux-td-dataset        "SMIRNOFF Coverage Torsion Set 1"                       \
+    --aux-td-dataset        "OpenFF Group1 Torsions"                                \
+    --aux-td-dataset        "OpenFF Group1 Torsions 2"                              \
+    --aux-td-dataset        "OpenFF Group1 Torsions 3"                              \
+    --aux-td-dataset        "Pfizer discrepancy torsion dataset 1"                  \
+    --aux-td-dataset        "OpenFF Gen3 Torsion Set v1.0"                          \
+    --aux-td-dataset        "OpenFF Amide Torsion Set v1.0"                         \
+    --aux-td-dataset        "OpenFF WBO Conjugated Series v1.0"                     \
+    --aux-td-dataset        "OpenFF DANCE 1 eMolecules t142 v1.0"                   \
+    --initial-forcefield    "../01_generate-forcefield/output/initial-force-field.offxml" \
+    --td-records-to-remove  "td_records_to_remove.dat"                              \
+    --additional-td-records "additional_td_records.json"                            \
+    --cap-size              5                                                       \
+    --cap-method            "pick_random"                                           \
+    --n-processes           8                                                       \
+    --output                "output/torsion-training-set.json"                      \
+    --output-parameter-smirks "output/training-torsion-smirks.json"                 \
+    --verbose
 
 date
