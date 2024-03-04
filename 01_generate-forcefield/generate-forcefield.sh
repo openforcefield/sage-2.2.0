ea91c4bed63db6390d9d3c19cc05ebf093df9917
Alexandra McIsaac <armcisaac@gmail.com>
2024-03-04 11:10:02 -0800

    adding files for FF without r4 heteroatom

--- 01_generate-forcefield/generate-forcefield.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-source ~/.bashrc
-conda activate fb_196_ic_0318
-
-python generate-forcefield.py                                           \
-    --force-field-name          "openff_unconstrained-2.1.0.offxml"     \
-    --output                    "output/initial-force-field.offxml"
