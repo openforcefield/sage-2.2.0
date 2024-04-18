6c68c5b4bb3b5311845da8a281ba6a7f85e4cf84
Alexandra McIsaac <armcisaac@gmail.com>
2024-04-17 20:54:52 -0700

    cleaning up filter scripts

--- 05_benchmark_forcefield/process_bm/filter_bm_smarts.py
+++ 05_benchmark_forcefield/process_bm/filter_bm_smarts.py
@@ -11,19 +11,15 @@ from openff.units import unit
 import pandas as pd
 from openff.toolkit import Molecule, ForceField
 
-# Sage 2.2 related
-#smarts_to_filter = [['[#7:1]-[#16X4:2](=[#8])(=[#8])~[#7:3]'],['[#7:1]-[#16X4:2](=[#8])(=[#8])~[*:3]'],['[#6;r3:1]1@[!#6;r3:2]@[#6;r3:3]1'],['[#6;r3:1]1@[#6;r3:2]@[#6;r3:3]1'], \
-#                    ['[#6;r4:1]1@[#6;r4:2]@[#6;r4:3]@[#6;r4]1'],['[#7;r4:1]'],['[#8;r4,#7;r4:1]'],['[#8;r4:1]'],['[r5:1]1@[#16;r5:2]@[r5:3]@[r5]@[r5]1'],['[r3:1]'],['[r4:1]'],\
-#                    ['[r5:1]'],['[r3,r4,r5:1]']]
 
-# Sage 2.1 related
-#smarts_to_filter = [['[*:1]-[#6X4:2]-[#6X3:3](~!@[#7X3])~!@[#7X3:4]'],['[*:1]-[#7X3+1:2]=,:[#6X3:3]-,:[*:4]'],["[#6X3:1](~!@[#7X3])(~!@[#7X3])~!@[#7X3:2]"], \
-#        ["[*:1]-[#6X4:2]-[#6X3:3](~[#8X1])~[#8X1:4]"],["[#1:1]-[#6X4:2]-[#6X3:3](~[#8X1])~[#8X1:4]"],["[#6X3:1]-[#6X4;r3:2]-[#6X3:3](~[#8X1])~[#8X1:4]"]]
-
-smarts_to_filter = [ ["[#6X4;r3:1]-;@[#6X4;r3:2]-[#6X3:3](~[#8X1])~[#8X1:4]"],["[#6X3:1]=[#6X3:2]-[#6X3:3](~[#8X1])~[#8X1:4]"],["[*:1]-[#6X3:2]-[#7X3:3](~[#8X1])~[#8X1:4]"],\
-        ["[*:1]=,:[#6X3:2]-[#7X3:3](~[#8X1])~[#8X1:4]"],["[*:1]~[#15:2]-[#6X4:3]-[*:4]"],["[*:1]-[#8X2:2]-[#15:3]~[*:4]"]]
 
 ff = ForceField('openff-2.1.0.offxml')
+
+# Sage 2.2 related
+smarts_to_filter = [['[#7:1]-[#16X4:2](=[#8])(=[#8])~[#7:3]'],['[#7:1]-[#16X4:2](=[#8])(=[#8])~[*:3]'],['[#6;r3:1]1@[!#6;r3:2]@[#6;r3:3]1'],['[#6;r3:1]1@[#6;r3:2]@[#6;r3:3]1'], \
+                    ['[#6;r4:1]1@[#6;r4:2]@[#6;r4:3]@[#6;r4]1'],['[#7;r4:1]'],['[#8;r4,#7;r4:1]'],['[#8;r4:1]'],['[r5:1]1@[#16;r5:2]@[r5:3]@[r5]@[r5]1'],['[r3:1]'],['[r4:1]'],\
+                    ['[r5:1]'],['[r3,r4,r5:1]']]
+
 industry_dataset = OptimizationResultCollection.parse_file('../datasets/OpenFF-Industry-Benchmark-Season-1-v1.1-filtered-charge-coverage.json')
 industry_entries = industry_dataset.entries['https://api.qcarchive.molssi.org:443/']
 industry_ids =[rec.record_id for rec in industry_entries]
@@ -37,7 +33,7 @@ for smarts_filter_pattern in smarts_to_filter:
 
         # First, find molecules that were filtered out by looking for record ID's that
         # appear only in the full dataset
-        print(industry_filtered.entries)
+        #print(industry_filtered.entries)
         filtered_entries = industry_filtered.entries['https://api.qcarchive.molssi.org:443/']
 
 
@@ -48,7 +44,7 @@ for smarts_filter_pattern in smarts_to_filter:
         print('Number of entries filtered out:          ',len(filtered_out_ids))
 
         #np.savetxt('all_ids_{}.txt'.format(''.join(smarts_filter_pattern)),industry_ids,fmt='%s')
-        np.savetxt('filter_ids/filtered_ids_{}.txt'.format(''.join(smarts_filter_pattern)),filtered_ids,fmt='%s')
+        np.savetxt('filter_ids/sage210/filtered_ids_{}.txt'.format(''.join(smarts_filter_pattern)),filtered_ids,fmt='%s')
         # np.savetxt('filter_ids/filtered_out_ids_{}.txt'.format(''.join(smarts_filter_pattern)),filtered_out_ids,fmt='%s')
     except KeyError:
         print('No matches for pattern ',smarts_filter_pattern)
